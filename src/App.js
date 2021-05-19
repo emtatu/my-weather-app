4da351f935983c2145b5b7c0cd2cce0b6808d7aa
emtatu <65530930+emtatu@users.noreply.github.com>
2021-05-19 23:14:48 +0200

    project init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { Container, Form, FormControl } from 'react-bootstrap';
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container className="Container"  >
+        <div className="Weather-app">
+          <Form inline className="Form" >
+            <div className="row input-container" >
+              <div className="col-9"  >
+                <FormControl type="text" placeholder="Add your city" className=" mr-sm-2 input-lenght" />
+              </div>
+              <div className="col-3" >
+                <Button type="submit" >Submit</Button>
+              </div>
+            </div>
+          </Form>
+          <div className="App-body">
+            <div>
+              <h3 className="Geo-location">Brussels</h3>
+              <div className="day-description">
+                <ul>
+                  <li> Tuesday 16:30</li>
+                  <li> Sunny</li>
+
+                </ul>
+              </div>
+              <div className="row  mt-3">
+                <div className="col-6" >
+
+                  <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"></img>
+                  <div className="degrees-content">
+                    <span className="degrees">12</span>
+                    <p className="temp-unit">°C | °F</p>
+                  </div>
+
+
+
+                </div>
+
+                <div className="col-6" >
+                  <ul>
+
+                    <li> humidity</li>
+                    <li> wind</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div >
+
+      </Container >
+
+
+    </div >
   );
 }
 
-export default App;
+
