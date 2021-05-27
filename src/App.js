fedd69bf05e24588815357e9827c0ca429a63fdf
emtatu <65530930+emtatu@users.noreply.github.com>
2021-05-27 11:12:39 +0200

    jason and api implemented

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import { Container, Form, FormControl } from 'react-bootstrap';
+import Weather from "./Weather"
+import { Container } from 'react-bootstrap';
 
 export default function App() {
   return (
-    <div className="App">
-      <Container className="Container"  >
-        <div className="Weather-app">
-          <Form inline className="Form" >
-            <div className="row input-container" >
-              <div className="col-9"  >
-                <FormControl type="text" placeholder="Add your city" className=" mr-sm-2 input-lenght" />
-              </div>
-              <div className="col-3" >
-                <Button type="submit" >Submit</Button>
-              </div>
-            </div>
-          </Form>
-          <div className="App-body">
-            <div>
-              <h3 className="Geo-location">Brussels</h3>
-              <div className="day-description">
-                <ul>
-                  <li> Tuesday 16:30</li>
-                  <li> Sunny</li>
+    <div className="App  ">
 
-                </ul>
-              </div>
-              <div className="row  mt-3">
-                <div className="col-6" >
+      <div className="bg_image">
+        <Container className="Container"  >
+          <Weather />
+          <footer>
+            <p className=" ">
+              This project was coded by <a href="https://www.linkedin.com/in/emiliatatu">Emilia Tatu</a> and is outsounrced.
+          </p>
 
-                  <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"></img>
-                  <div className="degrees-content">
-                    <span className="degrees">12</span>
-                    <p className="temp-unit">°C | °F</p>
-                  </div>
+          </footer>
+        </Container >
 
 
-
-                </div>
-
-                <div className="col-6" >
-                  <ul>
-
-                    <li> humidity</li>
-                    <li> wind</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div >
-
-      </Container >
-
-
+      </div>
     </div >
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import axios from "axios"
+import Button from 'react-bootstrap/Button';
+import { Form, FormControl } from 'react-bootstrap';
+import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
+
+
+
+export default function Weather() {
+    const [ready, setReady] = useState(false)
+    const [weatherData, setWeatherData] = useState({});
+    function handleResponse(response) {
+        console.log(response.data)
+        setWeatherData({
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            pressure: response.data.main.pressure,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            iconUrl: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+            maxTemperature: response.data.main.temp_max,
+            minTemperature: response.data.main.temp_min
+
+        })
+        setReady(true)
+    }
+    if (ready) {
+        return (
+            <div className="Weather-app">
+                <Form inline className="Form" >
+
+
+                    <div className="row input-container" >
+                        <div className="col-8"  >
+                            <FormControl type="text" placeholder="Add your city" className=" mr-sm-2 input-lenght" />
+                        </div>
+                        <div className="col-2" >
+                            <Button type="submit" className="submit-button" >Submit</Button>
+                        </div>
+                        <div className="col-2" >
+                            <Button type="submit" className="location-button" >Location</Button>
+                        </div>
+                    </div>
+                </Form>
+                <div className="App-body ">
+                    <div className="row">
+                        <div className="col-4 text-start ">
+                            <ul className="mt-3 pt-4">
+
+                                <li>Humidity: {weatherData.humidity} %</li>
+                                <li>Wind: {weatherData.wind}km/h</li>
+                                <li>Pressure: {weatherData.pressure}/ph</li>
+                            </ul>
+                        </div>
+                        <div className="col-5 text-start">
+                            <h3 className="geo-location text-center">{weatherData.city}</h3>
+                            <ul className="mt-2 pt-1">
+                                <li> Tuesday, 20 may </li>
+                                <li> 15:44</li>
+                                <li className="text-capitalize"> {weatherData.description} </li>
+
+                            </ul>
+                            <img src={weatherData.iconUrl} alt={weatherData.description} title={weatherData.description}></img>
+                            <div className="degrees-content">
+                                <span className="degrees">{Math.round(weatherData.temperature)}</span>
+                                <p className="temp-unit">°C | °F</p>
+                            </div>
+
+                        </div>
+
+                        <div className="col-3 text-start mt-3 pt-3">
+                            <ul>
+                                <li> Max-temp: {Math.round(weatherData.maxTemperature)}°C </li>
+                                <li> Min-temp: {Math.round(weatherData.minTemperature)}°C</li>
+                            </ul>
+                        </div>
+
+
+
+
+                    </div>
+                    <div className="future-predictions" >
+                        <div className="row">
+                            <div className="col">
+                                <ul>
+                                    <li>
+                                        14:00
+                    </li>
+
+                                    <li>img</li>
+                                    <li>
+                                        15°C
+                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div >
+
+
+            </div>
+        )
+    } else {
+        const apiKey = "81e9ca7b6afb78d66e95e98ae74c78a3";
+        let city = "Brussels";
+        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+        //const data = { data: { "coord": { "lon": 4.3488, "lat": 50.8504 }, "weather": [{ "id": 802, "main": "Clouds", "description": "scattered clouds", "icon": "03d" }], "base": "stations", "main": { "temp": 286.86, "feels_like": 286.01, "temp_min": 284.91, "temp_max": 288.07, "pressure": 1012, "humidity": 66, "sea_level": 1012, "grnd_level": 1009 }, "visibility": 10000, "wind": { "speed": 7.77, "deg": 279, "gust": 10.03 }, "clouds": { "all": 40 }, "dt": 1622029122, "sys": { "type": 1, "id": 1227, "country": "BE", "sunrise": 1622000389, "sunset": 1622057990 }, "timezone": 7200, "id": 2800866, "name": "Brussels", "cod": 200 } }
+        //handleResponse(response.data.main)
+        return "loading"
+
+    }
+
+
+}
