74ea94120f3785b945786b2f118b15479b36c239
emtatu <65530930+emtatu@users.noreply.github.com>
2021-05-31 11:51:08 +0200

    use correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconUrl: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             maxTemperature: response.data.main.temp_max,
             minTemperature: response.data.main.temp_min
 
