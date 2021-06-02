9c6cf33007a1493314d32141d228b9f9259d1fd3
emtatu <65530930+emtatu@users.noreply.github.com>
2021-06-02 15:32:29 +0200

    weather forecast temperature added

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import WeatherForecastDay from './WeatherForecastDay';
 import './WeatherForecast.css';
@@ -9,8 +9,14 @@ export default function WeatherForecast(props) {
 
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+
+
     function handleResponse(response) {
-        console.log(response.data);
+
         setForecast(response.data.daily);
         setLoaded(true);
 
@@ -22,9 +28,19 @@ export default function WeatherForecast(props) {
         return (
             <div className="weatherForecast" >
                 <div className="row">
-                    <div className="col">
-                        <WeatherForecastDay data={forecast[0]} />
-                    </div>
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                            return (
+                                <div className="col" key={index}>
+                                    <WeatherForecastDay data={dailyForecast} />
+                                </div>
+                            );
+                        } else {
+                            return null;
+                        }
+
+                    })}
+
                 </div>
             </div>
 
